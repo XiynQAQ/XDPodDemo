ee7f2187fee994be51bf83ae69a6896d5885cfa9
DingJia <dingjia-PengP@AP-2.local>
2017-05-16 11:32:23 +0800

    打印

--- XDPodDemo/ViewController.m
+++ XDPodDemo/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"这是个demo");
+    //NSLog(@"这是个demo");
 
 }
 
